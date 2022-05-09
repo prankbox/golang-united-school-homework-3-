3e853b53ff001bc51de19b0f5a6422d397799915
prankbox <edelweissco@mail.ru>
2022-05-09 12:32:46 +0300

    Update task02-slice.go

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,14 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	values := []string{}
+	keys := []int{}
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		values = append(values, input[k])
+	}
+	return values
 }
